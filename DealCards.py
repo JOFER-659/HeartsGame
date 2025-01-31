8342f8a144ead9d1f0834236a69d5181f0b70e5a
JOFER-659 <jhofer659@gmail.com>
2025-01-30 21:38:04 -0800

    Deck creation self contained function now, created DealCards

---
+++ DealCards.py
@@ -0,0 +1,17 @@
+#load libraries
+from DeckCreation import generate_deck
+from PlayerCreation import players, player_names
+#Create function to deal cards
+def Deal_Deck(players):
+    deck = generate_deck()
+    num_cards_per_player = len(deck) // len(players)
+
+    for i, player in enumerate(players):
+        hand = deck[i * num_cards_per_player:(i + 1) * num_cards_per_player]
+        player.hand = hand
+
+
+    for player in players:
+        print(player)
+
+Deal_Deck(players)
--- DeckCreation.py
+++ DeckCreation.py
@@ -1,16 +1,6 @@
 #Import libraries
 import random
 
-#Define values and suits
-values = list(range(2,15))
-suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
-face_cards = {
-    11: 'J',
-    12: 'Q',
-    13: 'K',
-    14: 'A',
-}
-
 #Create class 'Card'
 class Card:
     def __init__(self, suit, value):
@@ -18,18 +8,32 @@ class Card:
         self.value = value
 
     def __str__(self):
+        face_cards = {
+            11: 'J',
+            12: 'Q',
+            13: 'K',
+            14: 'A',
+        }
         value_str = face_cards.get(self.value, self.value)
         return f"{value_str} of {self.suit}"
 
 #Define generate deck function
-def generate_deck(values, suits):
+def generate_deck():
+    values = list(range(2, 15))
+    suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+    face_cards = {
+        11: 'J',
+        12: 'Q',
+        13: 'K',
+        14: 'A',
+    }
     deck = []
     for suit in suits:
         for value in values:
-            deck.append(Card(suit, value))
+            card_value = face_cards.get(value, value)
+            deck.append(Card(suit, card_value))
     return deck
 
 #Generate Deck and shuffle
-deck = generate_deck(values, suits)
+deck = generate_deck()
 random.shuffle(deck)
-
